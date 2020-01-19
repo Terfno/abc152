cdc5114cf51c561d807dea7c2f3eef5047acfb67
terfno <sueda.takahito@gmail.com>
2020-01-19 21:22:36 +0900

    :bug: ac

--- b.py
+++ b.py
@@ -5,10 +5,10 @@ b = int(input_ab[1])
 ab = ""
 ba = ""
 
-for i in range(b+1):
+for i in range(b):
   ab = ab + input_ab[0]
 
-for i in range(a+1):
+for i in range(a):
   ba = ba + input_ab[1]
 
 dic = [ab, ba]
